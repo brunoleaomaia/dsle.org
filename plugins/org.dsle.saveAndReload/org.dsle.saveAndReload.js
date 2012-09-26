eab3d232e9f912aedb70cced991360a91cb667cd
Bruno Maia <brunoleaomaia@gmail.com>
2012-09-26 18:30:16 -0300

    Plugin org.dsle.saveAndReload.js Added
    org.dsle.saveAndReload.js version 0.01

---
+++ plugins/org.dsle.saveAndReload/org.dsle.saveAndReload.js
@@ -0,0 +1,26 @@
+ide.registerPlugin({
+    name: 'org.dsle.saveAndReload',
+    file: 'org.dsle.saveAndReload.js',
+    description: 'Save the current file and reload the IDE.',
+    version: 0.01
+});
+
+ide.saveAndReload = function() {
+    ide.saveFile(function(){
+        document.location.href = 'index.html';
+    });  
+};
+
+ide.tools().menu.add({
+    icon: 'resources/icons/small/arrow_rotate_clockwise.png',
+    text: 'Save And Reload (Ctrl + Shift + R)',
+    handler: ide.saveAndReload
+});
+
+ide.createShortCut({
+    key: "r", 
+    ctrl: true, 
+    shift: true, 
+    alt: false, 
+    fn: ide.saveAndReload
+});
